eba10f6e360017c9d959f013460322f225f53114
Dawid <dyrcu15@gmail.com>
2016-07-28 13:54:15 +0200

    Send token to client

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express     = require('express'),
-    bodyParser  = require('body-parser'),
-	morgan      = require('morgan'),
-	mongoose    = require('mongoose'),
-	jwt         = require('jsonwebtoken'),
-	config      = require('./config'),
-	User        = require('./app/models/user');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+	morgan = require('morgan'),
+	mongoose = require('mongoose'),
+	jwt = require('jsonwebtoken'),
+	config = require('./config'),
+	User = require('./app/models/user');
 
 var app = express();
 
@@ -28,18 +28,42 @@ app.get('/users', (req, res) => {
 
 app.get('/setup', (req, res) => {
 	var user = new User({
-		name:'Dawid',
-		password:'dd'
-	 });
+		name: 'Dawid',
+		password: 'dd'
+	});
 
-	 user.save((err) => {
-		if(err){
+	user.save((err) => {
+		if (err) {
 			console.log(err);
 		}
 		res.end('Created!');
 	});
 });
 
+app.post('/authenticate', (req, res) => {
+
+	User.findOne({ name: req.body.name }, (err, user) => {
+		if (err) {
+			console.log(err);
+		}
+
+		if (!user) {
+			res.json({ authenticated: false });
+		} else {
+
+			if (user.password != req.body.password) {
+				res.json({ authenticated: false });
+			} else {
+
+				var token = jwt.sign(user, app.get('superSecret'));
+
+				res.json({ authenticated: true, token: token });
+
+			}
+		}
+	});
+
+});
 
 app.listen(8000);
 console.log('service in running');
