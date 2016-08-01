2cae7cc543a6227f4b5b38c56be9bf9a861b2963
Dawid Dyrcz <dyrcu15@gmail.com>
2016-08-01 20:19:44 +0200

    Improve modules location

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express'),
     bodyParser = require('body-parser'),
 	morgan = require('morgan'),
 	mongoose = require('mongoose'),
-	config = require('./config'),
-	userRouter = require('./app/routers/user_router'),
-	commonRouter = require('./app/routers/common_router');
+	config = require('config'),
+	userRouter = require('./lib/routers/user_router'),
+	commonRouter = require('./lib/routers/common_router');
 
 var app = express();
 
