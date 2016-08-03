c153b7cce418b825f7e63d16778433c2bd11676c
Dawid <dyrcu15@gmail.com>
2016-08-03 09:31:14 +0200

    Implement Sign In client logic

--- public/script.js
+++ public/script.js
@@ -1,9 +1,17 @@
 $(document).ready(() => {
 
     $('#signin').click(() => {
-        $.get('api/users?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyIkX18iOnsic3RyaWN0TW9kZSI6dHJ1ZSwiZ2V0dGVycyI6e30sIndhc1BvcHVsYXRlZCI6ZmFsc2UsImFjdGl2ZVBhdGhzIjp7InBhdGhzIjp7InBhc3N3b3JkIjoiaW5pdCIsIm5hbWUiOiJpbml0IiwiX2lkIjoiaW5pdCJ9LCJzdGF0ZXMiOnsiaWdub3JlIjp7fSwiZGVmYXVsdCI6e30sImluaXQiOnsicGFzc3dvcmQiOnRydWUsIm5hbWUiOnRydWUsIl9pZCI6dHJ1ZX0sIm1vZGlmeSI6e30sInJlcXVpcmUiOnt9fSwic3RhdGVOYW1lcyI6WyJyZXF1aXJlIiwibW9kaWZ5IiwiaW5pdCIsImRlZmF1bHQiLCJpZ25vcmUiXX0sImVtaXR0ZXIiOnsiZG9tYWluIjpudWxsLCJfZXZlbnRzIjp7fSwiX2V2ZW50c0NvdW50IjowLCJfbWF4TGlzdGVuZXJzIjowfX0sImlzTmV3IjpmYWxzZSwiX2RvYyI6eyJwYXNzd29yZCI6ImRkIiwibmFtZSI6ImRhd2lkIiwiX2lkIjoiNTdhMTkwYjJiZjQzNmRmMTg0ODZhNTMxIn0sIl9wcmVzIjp7IiRfX29yaWdpbmFsX3NhdmUiOltudWxsLG51bGxdLCIkX19vcmlnaW5hbF92YWxpZGF0ZSI6W251bGxdLCIkX19vcmlnaW5hbF9yZW1vdmUiOltudWxsXX0sIl9wb3N0cyI6eyIkX19vcmlnaW5hbF9zYXZlIjpbXSwiJF9fb3JpZ2luYWxfdmFsaWRhdGUiOltdLCIkX19vcmlnaW5hbF9yZW1vdmUiOltdfSwiaWF0IjoxNDcwMjA2MTM0fQ.4MQMM1I3nNdRFNOGeE35zP8-MH30iaae_t5nrYWGZmU', (data, status) => {
-            console.log(data);
-        });
+
+        var credentials = {
+            name: $('#email').val(),
+            password: $('#password').val(),
+        }
+
+        $.post('/authenticate',
+            credentials,
+            (data, status) => {
+                console.log(data);
+            });
 
         return false;
     });
