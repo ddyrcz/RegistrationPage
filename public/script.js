13662a5e03af1714056b5e36175ba82a490eab1a
Dawid <dyrcu15@gmail.com>
2016-08-03 13:19:15 +0200

    Store token in local storage

--- public/script.js
+++ public/script.js
@@ -1,18 +1,24 @@
-$(document).ready(() => {
+$(document).ready(() => {    
+
+    localStorage.removeItem('token');
 
     $('#signin').click(() => {
-
         var credentials = {
             name: $('#email').val(),
             password: $('#password').val(),
         }
-
         $.post('/authenticate',
             credentials,
-            (data, status) => {
-                console.log(data);
+            (data, status) => {                
+                localStorage.token = data.token;                
             });
+        return false;
+    });
 
+    $('#signup').click(() => {        
+        $.get(`/api/users?token=${localStorage.token}`, (users, status) => {
+            console.log(users);
+        });
         return false;
     });
 
