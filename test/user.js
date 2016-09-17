8a068102bfb5b877c2585806478dcbd17b7054f3
Dawid Dyrcz <dyrcu15@gmail.com>
2016-09-17 14:14:50 +0200

    FIX modules and add api/users POST test

--- test/user.js
+++ test/user.js
@@ -4,19 +4,45 @@ var chai = require('chai'),
     chaiHttp = require('chai-http'),
     server = require('../server');
 
-let should = chai.should();
-
-// var User = require('../lib/models/user');
+var should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('/GET api/users', () => {
-    it('it should GET all the users', (done) => {
-        chai.request(server)
-            .get('/api/users')
-            .end((err, res) => {
-                res.should.have.status(200);
-                done();
-            });
+var User = require('../lib/models/user');
+
+describe('Users', () => {
+    beforeEach((done) => {
+        User.remove({}, (err) => {
+            done();
+        });
+    });
+
+    describe('/POST api/users', () => {
+        it('it should create new user', (done) => {
+            var user = {
+                name:'ddyrcz', 
+                password:'pass'
+            };
+            chai.request(server)
+                .post('/api/users')
+                .send(user)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    //res.body.length.should.be.eql(1);
+                    done();
+                });
+        });
+    });
+
+    describe('/GET api/users', () => {
+        it('it should GET all the users', (done) => {
+            chai.request(server)
+                .get('/api/users')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.length.should.be.eql(0);
+                    done();
+                });
+        });
     });
 });
